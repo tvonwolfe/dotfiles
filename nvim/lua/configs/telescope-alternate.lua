5be171fb4516b53d816cd3b48f8c1a6d10353694
Tony Von Wolfe <tony@lovevery.com>
2023-04-18 11:53:51 +0200

    update alternate mappings

--- nvim/lua/configs/telescope-alternate.lua
+++ nvim/lua/configs/telescope-alternate.lua
@@ -3,9 +3,14 @@ local telescope_alternate = require 'telescope-alternate'
 telescope_alternate.setup {
   mappings = {
     { 'app/**/*.rb', { { 'spec/[1].rb', 'Test' } } }, -- alternate between file and spec
-    { 'app/repositories/[1]_repository.rb', 'Repository' }, -- search/create repositories for the model.
+    { 'app/models/*.rb', {
+        { 'app/repositories/[1]_repository.rb', 'Repository' },
+        { 'app/serializers/[1]_serializer.rb', 'Serializer' },
+      },
+    },
     { 'src/scripts/**/*.js', { { 'spec/**/[1].spec.js', 'Test' } } },
-    { 'src/scripts/**/*.ts', { { 'spec/**/[1].spec.ts', 'Test' } } }
+    { 'src/scripts/**/*.ts', { { 'spec/**/[1].spec.ts', 'Test' } } },
   },
   presets = { 'rails', 'rspec' },
+  open_only_one_with = 'vertical_split',
 }
