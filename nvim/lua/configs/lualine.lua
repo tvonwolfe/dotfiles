58986872245b6b492879d8d4abe7761430effdad
Tony Von Wolfe <tony@lovevery.com>
2023-06-29 12:07:06 +0200

    update lualine config

--- nvim/lua/configs/lualine.lua
+++ nvim/lua/configs/lualine.lua
@@ -1,9 +1,29 @@
 local vim = vim
 local lualine = require 'lualine'
 
-local get_ruby_version = function()
+local function get_ruby_version()
   local str = vim.fn['rvm#statusline_ft_ruby']()
   return str.match(str, '%[ruby%-(%d+.%d+.%d+)%]')
+end
+
+local function get_js_version()
+  return nil
+end
+
+local function get_lua_version()
+  return nil
+end
+
+LANGUAGE_LUT = {
+  eruby = get_ruby_version,
+  ruby = get_ruby_version,
+  javascript = get_js_version,
+  javascriptreact = get_js_version,
+  lua = get_lua_version,
+}
+
+local function get_language_version_fn()
+  return LANGUAGE_LUT[vim.bo.filetype]()
 end
 
 lualine.setup {
@@ -13,7 +33,7 @@ lualine.setup {
   },
   sections = {
     lualine_x = {
-      'filetype', get_ruby_version
+      'filetype', get_language_version_fn
     }
   }
 }
