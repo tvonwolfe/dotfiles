e36feb72541a5c7062ee86bf3477ddf5e60df939
Tony Von Wolfe <tony@lovevery.com>
2023-07-13 10:09:47 +0200

    update telescope-alternate config, tmux config

--- nvim/lua/configs/telescope-alternate.lua
+++ nvim/lua/configs/telescope-alternate.lua
@@ -2,12 +2,18 @@ local telescope_alternate = require 'telescope-alternate'
 
 telescope_alternate.setup {
   mappings = {
-    { 'app/**/*.rb', { { 'spec/[1].rb', 'Test' } } },
-    { 'app/models/(.*).rb', {
-      { 'app/repositories/[1]_repository.rb', 'Repository' },
-      { 'app/serializers/[1]_serializer.rb',  'Serializer' },
-      { 'spec/factories/[1:pluralize].rb',    'Factory' },
+    {
+      'app/**/(.*).rb',
+      { { 'spec/[1]_spec.rb', 'Test' } },
     },
+    { 'app/models/(.*).rb',
+      {
+        { 'app/repositories/[1]_repository.rb',             'Repository' },
+        { 'app/serializers/[1]_serializer.rb',              'Serializer' },
+        { 'app/helpers/[1:pluralize]_helper.rb',            'Helper' },
+        { 'app/controllers/**/[1:pluralize]_controller.rb', 'Controller' },
+        { 'spec/factories/[1:pluralize].rb',                'Factory' },
+      },
     },
     { 'src/scripts/**/*.js', { { 'spec/**/[1].spec.js', 'Test' } } },
     { 'src/scripts/**/*.ts', { { 'spec/**/[1].spec.ts', 'Test' } } },
