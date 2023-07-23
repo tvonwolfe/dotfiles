ebbca5763b4713261afe675563de550d80ea32d8
Tony Von Wolfe <tonyvonwolfe@gmail.com>
2023-07-23 09:38:58 +0200

    update gitignore files

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -94,6 +94,10 @@ return require('packer').startup(function(use)
     },
     'nvim-treesitter/nvim-treesitter',
     config = function() require 'configs.treesitter' end,
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
   }
 
   use {
