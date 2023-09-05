205a176f2098a59530b8411e651d7f1761a5983c
Tony Von Wolfe <tony@lovevery.com>
2023-09-05 16:33:40 +0200

    update auto-format command

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -2,6 +2,6 @@ local cmd = vim.cmd
 
 cmd 'autocmd! TermOpen * set nonumber norelativenumber nocursorline|startinsert'
 
-cmd [[cabbrev wq execute "lua vim.lsp.buf.format()" <bar> wq]]
+cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 
 cmd 'autocmd! VimResized * wincmd ='
