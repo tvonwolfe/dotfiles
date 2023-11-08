07ad8888c624032f65280b9cdf5cfcc69bf0d74e
Tony Von Wolfe <tony@lovevery.com>
2023-11-08 11:19:59 +0100

    comment out keymaps that are causing error, add TODO comment to figure it out later

--- nvim/lua/configs/kiwi.lua
+++ nvim/lua/configs/kiwi.lua
@@ -5,7 +5,8 @@ if not kiwi_ok then return end
 
 kiwi.setup()
 
-keymap.set('n', '<leader>ww', kiwi.open_wiki_index, {})
-keymap.set('n', '<leader>wd', kiwi.open_diary_index, {})
-keymap.set('n', '<leader>wn', kiwi.open_diary_new, {})
-keymap.set('n', '<leader>wt', kiwi.todo.toggle, {})
+keymap.set('n', '<leader>ww', kiwi.open_wiki_index)
+-- TODO: figure out why these functions are `nil`
+-- keymap.set('n', '<leader>wd', kiwi.open_diary_index)
+-- keymap.set('n', '<leader>wn', kiwi.open_diary_new)
+keymap.set('n', '<leader>wt', kiwi.todo.toggle)
