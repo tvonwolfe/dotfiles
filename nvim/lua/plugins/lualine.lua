c648b0119118d6761c1c7399f50fd771c52a72b6
Tony Von Wolfe <tony@lovevery.com>
2024-10-24 13:31:07 -0600

    remove interpreter version from lualine, caused too much lag in Next.js projects

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,34 +1,11 @@
 local lualine = require 'lualine'
 local lsp_progress = require 'lsp-progress'
 
-local function get_ruby_version()
-  local str = vim.fn['rvm#statusline_ft_ruby']()
-  return str.match(str, '%[ruby%-(%d+.%d+.%d+)%]')
-end
-
-local function get_node_version()
-  local str = io.popen('node -v'):read('*a')
-  return str:gsub('[\n\r]', '')
-end
-
-local function get_lua_version()
-  return nil
-end
-
-LANGUAGE_LUT = {
-  eruby = get_ruby_version,
-  ruby = get_ruby_version,
-  javascript = get_node_version,
-  javascriptreact = get_node_version,
-  typescript = get_node_version,
-  typescriptreact = get_node_version,
-}
-
-local function get_language_version_fn()
-  return LANGUAGE_LUT[vim.bo.filetype]()
-end
-
 lsp_progress.setup()
+
+local function get_lsp_progres()
+  return lsp_progress.progress()
+end
 
 lualine.setup {
   options = {
@@ -37,7 +14,7 @@ lualine.setup {
   },
   sections = {
     lualine_x = {
-      function() return lsp_progress.progress() end, 'filetype', get_language_version_fn
+      get_lsp_progres, 'filetype',
     }
   }
 }
