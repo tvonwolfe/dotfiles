f671edea9a3bed974f27858db39205ff7740ddeb
Tony Von Wolfe <tonyvonwolfe@protonmail.com>
2025-11-30 20:51:56 -0700

    update markdown ft settings

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,10 +1,20 @@
 -- Turn on spellcheck for named buffers only.
 if vim.fn.expand('%') ~= '' then
-  vim.cmd [[setlocal spell spelllang=en_us]]
+  vim.opt_local.spell = true
+  vim.opt_local.spelllang = { "en_us" }
 end
 
--- re-format upon exiting insert mode.
-vim.cmd [[setlocal formatoptions+=a]]
-vim.cmd [[setlocal conceallevel=0]]
+vim.cmd [[ IlluminatePauseBuf ]]
 
-vim.cmd "IlluminatePauseBuf"
+vim.opt_local.textwidth = 0
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+vim.opt_local.breakindent = true
+vim.opt_local.signcolumn = 'no'
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
+
+vim.keymap.set("n", "j", "gj", { buffer = true })
+vim.keymap.set("n", "k", "gk", { buffer = true })
+vim.keymap.set("n", "0", "g0", { buffer = true })
+vim.keymap.set("n", "$", "g$", { buffer = true })
