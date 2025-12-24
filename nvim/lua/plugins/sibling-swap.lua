dd3cecc542304cf365a039ba3b7dd9f583cd49ed
Tony Von Wolfe <tonyvonwolfe@protonmail.com>
2025-12-24 11:33:30 -0700

    update sibling-swap config

--- nvim/lua/plugins/sibling-swap.lua
+++ nvim/lua/plugins/sibling-swap.lua
@@ -1,8 +1,9 @@
 return {
   'Wansmer/sibling-swap.nvim',
-  opts = { use_default_keymaps = false },
-  keys = {
-    { '<leader>h', function() require('sibling-swap').swap_with_left() end,  { desc = 'move TS node to the left' } },
-    { '<leader>l', function() require('sibling-swap').swap_with_right() end, { desc = 'move TS node to the right' } },
-  }
+  dependencies = { 'nvim-treesitter' },
+  config = function() require('sibling-swap').setup() end
+  -- keys = {
+  --   { '<leader>h', function() require('sibling-swap').swap_with_left() end,  { desc = 'move TS node to the left' } },
+  --   { '<leader>l', function() require('sibling-swap').swap_with_right() end, { desc = 'move TS node to the right' } },
+  -- }
 }
