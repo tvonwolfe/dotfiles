bc31f0502783bb5ab557c21450808d869b6fc196
Tony Von Wolfe <tonyvonwolfe@protonmail.com>
2026-04-06 17:29:42 -0600

    update waybar config, add easy-dotnet nvim plugin

---
+++ nvim/lua/plugins/easy-dotnet.lua
@@ -0,0 +1,8 @@
+-- lazy.nvim
+return {
+  "GustavEikaas/easy-dotnet.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim', },
+  config = function()
+    require("easy-dotnet").setup()
+  end
+}
