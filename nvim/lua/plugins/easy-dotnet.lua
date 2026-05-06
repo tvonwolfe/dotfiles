6fc61d4784f74a5a89a93deb7bae8f3222f1e079
Tony Von Wolfe <tonyvonwolfe@protonmail.com>
2026-05-05 19:10:47 -0600

    rookie move

--- nvim/lua/plugins/easy-dotnet.lua
+++ nvim/lua/plugins/easy-dotnet.lua
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
--- lazy.nvim
-=======
->>>>>>> 4ffd867 (c# support, treesitter config update)
 return {
   "GustavEikaas/easy-dotnet.nvim",
   dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim', },
